cb2674c6206ae50b70b18fff22dda343e6c0a2c6
William <menzongwei@126.com>
2018-07-12 16:05:23 +0800

    sent api request and show data in page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,18 @@
-Page({})
+Page({
+  data: {
+    nowTemp: '14°',
+    nowWheater: '阴天'
+  },
+  onLoad(){
+    console.log('page load success');
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data:{
+        city:'北京市'
+      },
+      success: res => {
+        console.log(res);
+      }
+    })
+  }
+})
