19b5b6f04429745cb432fa6a2cd1e97faacd651a
William <menzongwei@126.com>
2018-07-16 15:05:52 +0800

    second page finished

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,28 +1,57 @@
+const dayMap = ['星期日','星期一','星期二','星期三','星期四','星期五','星期六']
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    weekWeather:[1,2,3,4,5,6,7]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function () {
+    this.getWeekWeather()
+  },
+  onPullDownRefresh(){
+    this.getWeekWeather(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  getWeekWeather(callback){
     wx.request({
-      url: 'https://test-miniprogram.com/api/weather/now',
+      url: 'https://test-miniprogram.com/api/weather/future',
       data: {
         city: '北京市',
         time: new Date().getTime()
       },
       success: res => {
-        console.log(res);
+        let result = res.data.result;
+        this.setWeekWeather(result);
+      },
+      complete:()=>{
+        callback && callback()
       }
     })
   },
-
+  setWeekWeather(result){
+    let weekWeather = [];
+    for(let i = 0;i<7;i++){
+      let date = new Date();
+      date.setDate(date.getDate()+i)
+      weekWeather.push({
+        day:dayMap[date.getDay()],
+        date:`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`,
+        temp: `${result[i].minTemp}°-${result[i].maxTemp}°`,
+        iconPath:'/images/'+result[i].weather+'-icon.png'
+      })
+    }
+    weekWeather[0].day='今天'
+    this.setData({
+      weekWeather
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
