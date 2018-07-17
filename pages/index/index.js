8a5ac21385031874fd27e49afd5394314b41711f
William <menzongwei@126.com>
2018-07-17 17:00:12 +0800

    onshow setting

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,6 +14,16 @@ const weatherColorMap = {
   'heavyrain': '#c5ccd0',
   'snow': '#aae1fc'
 }
+//使用有含义的变量名来增加可读性
+//能够使用函数名和变量名表达的信息，不应该用注释来表达。注释往往是用来传递一些设计的想法或解释不常见的实现方法
+const UNPROMPTED = 0
+const UNAUTHORIZED = 1
+const AUTHORIZED = 2
+
+const UNPROMPTED_TIPS = "点击获取当前位置"
+const UNAUTHORIZED_TIPS = "点击开启位置权限"
+const AUTHORIZED_TIPS = ""
+
 var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
 
 Page({
@@ -25,7 +35,8 @@ Page({
     todayTemp:'',
     todayDate:'',
     city:'广州市',
-    getLocationTips:'点击获取当前位置'
+    locationAuthType: UNPROMPTED,
+    locationTipsText: UNPROMPTED_TIPS
   },
   getNow(callback){
     wx.request({
@@ -55,6 +66,19 @@ Page({
       key: 'EAXBZ-33R3X-AA64F-7FIPQ-BY27J-5UF5B'
     });
     this.getNow();
+  },
+  onShow(){
+    wx.getSetting({
+      success: res=>{
+        let auth = res.authSetting['scope.userLocation']
+        if(auth && this.data.locationAuthType !== AUTHORIZED){//权限从无到有
+          this.setData({
+            locationAuthType:AUTHORIZED,
+            locationTipsText:AUTHORIZED_TIPS
+          })
+        }
+      }
+    })
   },
   setHours(result){
     let forecast = result.forecast;
@@ -97,8 +121,18 @@ Page({
     })
   },
   onTapLocation(){
+    if(this.data.locationAuthType === UNAUTHORIZED)
+    wx.openSetting()
+    else
+    this.getLocation()
+  },
+  getLocation(){
     wx.getLocation({
       success:res=>{
+        this.setData({
+          locationAuthType: AUTHORIZED,
+          locationTipsText: AUTHORIZED_TIPS
+        })
         this.qqmapsdk.reverseGeocoder({
           location:{
             latitude:res.latitude,
@@ -111,6 +145,12 @@ Page({
               getLocationTips:''
             })
           }
+        })
+      },
+      fail:()=>{
+        this.setData({
+          locationAuthType: UNAUTHORIZED,
+          locationTipsText: UNAUTHORIZED_TIPS
         })
       }
     })
